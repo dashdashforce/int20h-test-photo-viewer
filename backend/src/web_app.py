0479c5326df78b9a9ab53cbc85ebc1b2e296c494
Oleg Lipskiy <lolego1601@gmail.com>
2019-02-05 23:35:25 +0200

    feat: add base graphql schema

--- backend/src/web_app.py
+++ backend/src/web_app.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, division, print_function
 
 from tornado.web import Application
 from .graphql import GraphQLHandler
+from .schema import schema
 
 
 class MainApplicationHandler(GraphQLHandler):
@@ -11,6 +12,11 @@ class MainApplicationHandler(GraphQLHandler):
     def initialize(self, opts):
         super(MainApplicationHandler, self).initialize()
         self.opts = opts
+        self._schema = schema
+
+    @property
+    def schema(self):
+        return self._schema
 
 
 class PhotoViewerApiApplication(Application):
