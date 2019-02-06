c5928ce31e3c3db23db69e40622e1eb37eec19a0
Oleg Lipskiy <lolego1601@gmail.com>
2019-02-06 19:16:58 +0200

    feat: add faceplusplus service base implementation

---
+++ backend/src/services/faceplusplus_service.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import os
+from urllib.parse import urlencode
+from dotenv import load_dotenv, find_dotenv
+from tornado.httpclient import AsyncHTTPClient, HTTPRequest
+from tornado.escape import json_decode, json_encode
+
+
+load_dotenv(find_dotenv())
+
+
+class FacePlusPlusService:
+
+    def __init__(self):
+        self.async_http_client = AsyncHTTPClient()
+
+    def _build_photo_detect_uri(self, photo_uri):
+        api_uri = os.getenv("FACEPLUSPLUS_URI")
+        api_method = os.getenv("FACEPLUSPLUS_DETECT_METHOD")
+        return "{root}/{method}".format(root=api_uri, method=api_method)
+
+    def _build_request(self, photo_uri):
+        uri = self._build_photo_detect_uri(photo_uri)
+        api_key = os.getenv("FACEPLUSPLUS_API_KEY")
+        api_secret = os.getenv("FACEPLUSPLUS_API_SECRET")
+        attributes = ['emotion', 'gender']
+
+        body = (
+            ('api_key', api_key),
+            ('api_secret', api_secret),
+            ('image_url', photo_uri),
+            ('return_attributes', ','.join(attributes))
+        )
+        return HTTPRequest(
+            url=uri,
+            method='POST',
+            body=urlencode(body)
+        )
+
+    async def get_photo_face_data(self, photo_uri):
+        request = self._build_request(photo_uri)
+        response = await self.async_http_client.fetch(request)
+        return json_decode(response.data)
