64204c23c65f8514a8cc12ddf9eb537747cd9c72
Oleg Lipskiy <lolego1601@gmail.com>
2019-02-07 18:14:18 +0200

    refactor: move fetch_photos to src folder

--- backend/src/fetch_photos.py
+++ backend/src/fetch_photos.py
@@ -1,4 +1,4 @@
-from src.services import service_locator
+from .services import service_locator
 from pprint import pprint
 flickr_service = service_locator.flickr_service
 
@@ -6,7 +6,7 @@ page = 1
 
 response = flickr_service.fetch_sync(page)
 pprint(response)
-flickr_service.save_photos(response['photoset']['photo'])
+flickr_service._save_photos(response['photoset']['photo'])
 
 for i in range((page+1), response.photoset.pages):
     flickr_service.fetch_sync(page)
