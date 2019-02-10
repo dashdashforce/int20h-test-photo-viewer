3c939d7be9619a2bae958967c34cb8b720033344
Oleg Lipskiy <lolego1601@gmail.com>
2019-02-10 21:48:19 +0200

    feat: add background update checking task

--- backend/app/jobs/photos_update_check_job.py
+++ backend/app/jobs/photos_update_check_job.py
@@ -2,11 +2,16 @@
 from tornado import ioloop
 from tornado.log import app_log
 
+from app.clients import FlickrApiClient
+from app.repository import PhotosRepository
+
 
 class PhotosUpdateCheckJob(ioloop.PeriodicCallback):
 
     def __init__(self):
-        self.period = 5000
+        self.period = 30000
+        self.repository = PhotosRepository()
+        self.client = FlickrApiClient()
         super(PhotosUpdateCheckJob, self).__init__(
             self.fetch_photos,
             self.period
@@ -16,5 +21,13 @@ class PhotosUpdateCheckJob(ioloop.PeriodicCallback):
         TODO implement flickr api the most recent photo fetching
     """
     async def fetch_photos(self):
-        pass
-        # app_log.debug("Fetching photos")
+        latest_photo = await self.repository.get_photos(1, None)
+        page = 1
+        search_page = (await self.client.fetch_photos_by_tag(page, 20))['photo']
+        if search_page and latest_photo:
+            while search_page[0]['dateupload'] > latest_photo[0]['dateupload']:
+                app_log.info('New photo detected')
+                page += 1
+                await self.repository.save_photos(search_page)
+                search_page = (await self.client.fetch_photos_by_tag(
+                    page, 20))['photo']
